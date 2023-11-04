b75ac3cafd7cb0a844cef0f3029d9ac3970846c6
Josh Duschik <josh.duschik@gmail.com>
2023-11-04 16:04:07 -0700

    utils/ routing complete

---
+++ server/api/utils.js
@@ -0,0 +1,9 @@
+const requireUser = (req, res, next) => {
+  if (req.userId) {
+    next();
+  } else {
+    res.status(401).send({ message: 'User unauthorized '});
+  }
+};
+
+module.exports = { requireUser };
--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
 require('dotenv').config();
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const path = require('path');
-
 const app = express();
 const PORT = 3005;
 
 app.use(require('body-parser').json());
 app.use(require('morgan')('dev'));
+
+app.use((req, res, next) => {
+  const auth = req.headers.authorization;
+  const token = auth?.startsWith('Bearer ') ? auth.slice(7) : null;
+  try {
+    const { id } = jwt.verify(token, process.env.JWT);
+    req.userId = id;
+  } catch (error) {
+    req.userId = null;
+  }
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('working');
