9c5b63b68cf16a305813f10a309fe65bb436aa9b
Josh Duschik <josh.duschik@gmail.com>
2023-11-04 11:59:51 -0700

    schema migrated

---
+++ prisma/migrations/20231104185805_innit/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "taskTitle" VARCHAR(255) NOT NULL,
+    "taskDescription" TEXT,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
