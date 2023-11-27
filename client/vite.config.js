2f641fe859c3e403d89d883e78bf861e0d18e0b3
Josh Duschik <josh.duschik@gmail.com>
2023-11-27 16:12:54 -0700

    typo fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost10000',
+      '/api': 'http://localhost:10000',
       '/auth': 'http://localhost:10000'
     },
   },
