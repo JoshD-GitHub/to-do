4c48e5f4de5243f2a90a5328da30fb9627d0ce43
Josh Duschik <josh.duschik@gmail.com>
2023-11-27 15:53:50 -0700

    config update

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5432',
-      '/auth': 'http://localhost:5432'
+      '/api': 'http://localhost3005',
+      '/auth': 'http://localhost:3005'
     },
   },
 });
